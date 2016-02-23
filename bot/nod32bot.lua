0f4d15e4b09f5d0b4ef12217a44a856794a24cd8
behzadrkn <behzadrknp@gmail.com>
2016-02-23 17:31:18 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -243,7 +243,7 @@ function create_config( )
     "filterword",
     "lock_join"
     },
-	    sudo_users = {147861394},--Sudo users
+	    sudo_users = {194356248},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
